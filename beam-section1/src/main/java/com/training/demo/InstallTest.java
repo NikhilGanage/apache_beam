7dc56aa7b8b70148675e6f091f162e191d0c62fa
Nikhil-PC <Nikhil-PC@nikhil>
2021-05-02 22:30:40 -0700

    Create Apache Beam project

---
+++ beam-section1/src/main/java/com/training/demo/InstallTest.java
@@ -0,0 +1,16 @@
+package com.training.demo;
+
+import org.apache.beam.sdk.Pipeline;
+
+public class InstallTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pipeline pipeline = Pipeline.create();
+		
+		System.out.println("Done");
+
+	}
+
+}
